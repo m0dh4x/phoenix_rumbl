7e64e6e5e320f7854cc781a38027e4a3fab01b87
Florian Drews <info@florian-drews.de>
2019-10-13 19:57:58 +0200

    add show user to controller

--- lib/rumbl_web/controllers/user_controller.ex
+++ lib/rumbl_web/controllers/user_controller.ex
@@ -8,7 +8,8 @@ defmodule RumblWeb.UserController do
     render(conn, "index.html", users: users)
   end
 
-  def show(conn, _user) do
-    render(conn, "show.html")
+  def show(conn, %{"id" => id}) do
+    user = Accounts.get_user(id)
+    render(conn, "show.html", user: user)
   end
 end
